c25217e9f5613b4df2bd4b873ace2023c80e112b
Eveline Kooijman <e.kooijman@gmail.com>
2017-09-28 15:58:09 +0200

    4.2 update database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name: "Rotan Chair", description: "Round Rotan Chair", image_url: "prod-xl/prod2.jpg")
+Product.create(name: "Stool", description: "Made of plain wood", image_url: "prod-xl/prod3.jpg")
+Product.create(name: "Vases", description: "Milky way design", image_url: "prod-xl/prod4.jpg")
+Product.create(name: "Abstract Lamp", description: "Ceiling light", image_url: "prod-xl/prod5.jpg")
+Product.create(name: "Colorful Sofa", description: "From the Sofa Designers", image_url: "prod-xl/prod6.jpg")
+Product.create(name: "Clean Black", description: "Modern polymere chair", image_url: "prod-xl/prod7.jpg")
+Product.create(name: "Table", description: "clean and crisp design", image_url: "prod-xl/prod8.jpg")
