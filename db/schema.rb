91ab6398a0a73408c79c2b7f3d1373895e7e9528
Eveline Kooijman <e.kooijman@gmail.com>
2017-10-02 08:41:12 +0200

    4.2 Added landing page with dynamic generated images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928134537) do
+ActiveRecord::Schema.define(version: 20171002063644) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170928134537) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "price"
+    t.string "teaser"
   end
 
 end
