c9d2737629de4401d87fbce6d2542c25c7bf7c70
Eveline Kooijman <e.kooijman@gmail.com>
2017-10-05 18:37:43 +0200

    rollback migration create-categories

--- db/migrate/20171005135458_create_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategories < ActiveRecord::Migration[5.1]
-  def change
-    create_table :categories do |t|
-      t.string :name
-      t.integer :product_id
-    end
-    add_index :categories, :product_id
-  end
-end
