81b7d65fcc882155803982f075cad98e3bf492a3
Eveline Kooijman <e.kooijman@gmail.com>
2017-10-05 18:35:06 +0200

    remove create categories

--- db/migrate/20171005150615_create_article_id_in_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateArticleIdInCategories < ActiveRecord::Migration[5.1]
-  def change
-    create_table :article_id_in_categories do |t|
-      t.integer :article_id
-    end
-    add_index :article_id_in_categories, :article_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005135458) do
+ActiveRecord::Schema.define(version: 20171005132823) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,12 +18,6 @@ ActiveRecord::Schema.define(version: 20171005135458) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "product_id"
-    t.index ["product_id"], name: "index_categories_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
