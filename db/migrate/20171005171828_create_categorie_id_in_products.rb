29566032c453c7e2ab6cdda507d27e72ffd7b2c6
Eveline Kooijman <e.kooijman@gmail.com>
2017-10-09 20:11:25 +0200

    delete wrong migrations for categories

--- db/migrate/20171005171828_create_categorie_id_in_products.rb
+++
@@ -1,6 +0,0 @@
-class CreateCategorieIdInProducts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :products, :categorie_id, :integer
-  end
-  add_index :products, :categorie_id
-end
--- db/migrate/20171009172942_remove_categorie_id_from_products.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategorieIdFromProducts < ActiveRecord::Migration[5.1]
-  # def change
-  #   remove_column :products, :categorie_id, :integer
-  # end
-end
