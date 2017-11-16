007fa8f051e184c0eca9e4de8846b97236ead75b
Eveline Kooijman <e.kooijman@gmail.com>
2017-11-16 15:26:44 +0100

    C.F 5.3 | feature: SendGrid addon heroku, generate mailer, add HTML email template for contact form

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'donotreply@moebi.herokuapp.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+	default from: 'donotreply@moebi.herokuapp.com'
+
+	def contact_form(email, name, message)
+		@message = message
+		mail( from: email,
+			  to: "e.kooijman@gmail.com",
+			  subject: "A new contact form message from #{name}")
+	end
+end
