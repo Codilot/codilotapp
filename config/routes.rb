bd27caa4baf8a330c64ffe0420175c328d044a26
Eveline Kooijman <e.kooijman@gmail.com>
2017-11-23 12:28:15 +0100

    restore routes, delete confirmation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-
-  devise_for :users, path: '', 
-                     controllers: { confirmations: 'confirmations' }, 
-                     path_names: { sign_in: 'login',
-                                   sign_out: 'logout'}                   
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   resources :users
   resources :articles, :products
