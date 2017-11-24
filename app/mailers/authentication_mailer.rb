fc1495de2b29866a9aecc563e9d7fde3a80f3279
Eveline Kooijman <e.kooijman@gmail.com>
2017-11-24 13:08:02 +0100

    Feature: delete old  url opions in production and development environment

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -1,6 +1,6 @@
 class AuthenticationMailer < Devise::Mailer   
   helper :application # gives access to all helpers defined within `application_helper`.
-  include DefaultUrlOptions
+  include DefaultUrlOptions # default url options under app/concerns/default_url_options.rb
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,10 +3,6 @@ Rails.application.configure do
 
   #moebi default url options 
   #config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  #config.action_mailer.delivery_method = :smtp
-  
-  #moebi default url options
-  config.action_mailer.default_url_options = { host: 'moebi.herokuapp.com' }
 
   #moebi smtp settings
   config.action_mailer.delivery_method = :smtp
