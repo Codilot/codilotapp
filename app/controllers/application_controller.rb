913c00faeaefefb1728ca4c56e738bf547cccf04
Eveline Kooijman <e.kooijman@gmail.com>
2017-11-27 14:51:15 +0100

    CF 5.5 | Feature: fix  rescue redirect in Application controller, Authorization, user authorization on user id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
-  
-=======
 
-  #redirect  after succesful login
-  # def after_sign_in_path_for(resource)
-  #   static_pages_dining_path
-  # end
->>>>>>> master
 end
