ac0370b78452bd062c90d7a33e415acad0bc13de
Eveline Kooijman <e.kooijman@gmail.com>
2017-11-28 11:16:37 +0100

    Feature: add First name and Last name registration to Devise registration Cntroller. Name can be added under edit registration view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,4 +8,14 @@ class RegistrationsController < Devise::RegistrationsController
     devise_registrations_confirmation_is_sent_path
   end
 
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+
 end
