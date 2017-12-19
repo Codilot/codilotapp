ffa394c3fc9cfeb99d6008b811aaa43205977b74
Eveline Kooijman <e.kooijman@gmail.com>
2017-12-19 14:47:30 +0100

    CF 5.10 | add migration adminflag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130112916) do
+ActiveRecord::Schema.define(version: 20171219131159) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20171130112916) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.boolean "admin", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
