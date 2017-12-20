48ac83496ccfc21b8787de150523498e45aecd4e
Eveline Kooijman <e.kooijman@gmail.com>
2017-12-20 16:29:30 +0100

    CF 5.9 | Feature: Welcome Mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  validates_format_of :first_name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
-  validates_format_of :last_name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/
    
   has_many :orders
   has_many :comments
