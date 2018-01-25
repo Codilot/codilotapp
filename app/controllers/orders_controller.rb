ecf38e54d03996aac5311f40f3c967df9a7ebe19
Eveline Kooijman <e.kooijman@gmail.com>
2018-01-25 12:59:48 +0100

    CF 4.6 | Add order controller actions, add order views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
   
   def index
+    @orders = Order.includes(:product).all
   end
 
   def show
@@ -14,6 +16,17 @@ class OrdersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def order_params
+    params.require(:order).permit(:user_id, :product_id, :total)
   end
 
 end
