3e38de0e15976c39886517e639953cd4dfe693f6
Eveline Kooijman <e.kooijman@gmail.com>
2018-02-15 11:47:24 +0100

    DRY tests product and user model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,69 @@
 require 'rails_helper'
 
-describe Product do
+RSpec.describe Product, type: :model do
+  
+  context "is not valid without a name" do 
+    let(:product) {Product.new(teaser: "great new lamp", 
+                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+                               price: 200.50
+    )}
+    it "returns invalid without a name" do 
+      expect(product).not_to be_valid
+    end
+  end
+
+  context "is not valid without a teaser" do 
+    let(:product) {Product.new(name: "lamp", 
+                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+                               price: 200.50
+    )}
+    it "returns invalid without a teaser" do 
+      expect(product).not_to be_valid
+    end
+  end
+
+  context "is not valid with a teaser of more than 40 characters" do 
+    let(:product) {Product.new(name: "lamp",
+                               teaser: "great new lamp that shines light and bright and extensively for a longer period of time", 
+                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+                               price: 200.50
+    )}
+    it "returns invalid when teaser has more than 40 characters" do 
+      expect(product).not_to be_valid
+    end
+  end
+
+  context "is not valid without a description" do 
+    let(:product) {Product.new(name: "lamp", 
+                               teaser: "great new lamp",
+                               price: 200.50
+    )}
+    it "returns invalid without a description" do 
+      expect(product).not_to be_valid
+    end
+  end
+
+  context "is not valid with a description of less than 200 characters" do 
+    let(:product) {Product.new(name: "lamp",
+                               teaser: "great new lamp", 
+                               description: "Too short description",
+                               price: 200.50
+    )}
+    it "returns invalid when description has less than 200 characters" do 
+      expect(product).not_to be_valid
+    end
+  end
+
+  context "is not valid without a price" do 
+    let(:product) {Product.new(name: "lamp", 
+                               teaser: "great new lamp",
+                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+    )}
+    it "returns invalid without a price" do 
+      expect(product).not_to be_valid
+    end
+  end
+
   context "when product has comments" do
     let(:product) {Product.create!(name: "lamp", 
                                    teaser: "great new lamp", 
@@ -20,63 +83,39 @@ describe Product do
     it "returns the average rating of all comments" do 
       expect(product.average_rating).to eq(3)
     end 
-  end  
-  context "is not valid without a name" do 
-    let(:product) {Product.new(teaser: "great new lamp", 
-                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
-                               price: 200.50
-    )}
-    it "returns invalid without a name" do 
-      expect(product).not_to be_valid
+  end 
+
+  describe "search products for a searchterm" do 
+    before do
+      @product1 = Product.create(name: "basic lamp", 
+                                teaser: "great new lamp", 
+                                description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+                                price: 99.99
+                                )
+      @product2 = Product.create(name: "basic lamp hood", 
+                                teaser: "hood of the basic lamp", 
+                                description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+                                price: 9.99
+                                )
+      @product3 = Product.create(name: "different lamp", 
+                                teaser: "a whole different lamp", 
+                                description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
+                                price: 199.99
+                                )    
+    end
+    context "when a match is found" do
+      it "returns products that match the search term" do 
+        expect(Product.search("basic")).to include(@product1, @product2)
+        expect(Product.search("basic")).not_to include(@product3)
+      end
+    end
+    context "when no match is found" do
+      it "returns an empty collection" do 
+        expect(Product.search("xxx")).to be_empty
+      end
     end
   end
-  context "is not valid without a teaser" do 
-    let(:product) {Product.new(name: "lamp", 
-                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
-                               price: 200.50
-    )}
-    it "returns invalid without a teaser" do 
-      expect(product).not_to be_valid
-    end
-  end
-  context "is not valid with a teaser of more than 40 characters" do 
-    let(:product) {Product.new(name: "lamp",
-                               teaser: "great new lamp that shines light and bright and extensively for a longer period of time", 
-                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
-                               price: 200.50
-    )}
-    it "returns invalid when teaser has more than 40 characters" do 
-      expect(product).not_to be_valid
-    end
-  end
-  context "is not valid without a description" do 
-    let(:product) {Product.new(name: "lamp", 
-                               teaser: "great new lamp",
-                               price: 200.50
-    )}
-    it "returns invalid without a description" do 
-      expect(product).not_to be_valid
-    end
-  end
-  context "is not valid with a description of less than 200 characters" do 
-    let(:product) {Product.new(name: "lamp",
-                               teaser: "great new lamp", 
-                               description: "Too short description",
-                               price: 200.50
-    )}
-    it "returns invalid when description has less than 200 characters" do 
-      expect(product).not_to be_valid
-    end
-  end
-  context "is not valid without a price" do 
-    let(:product) {Product.new(name: "lamp", 
-                               teaser: "great new lamp",
-                               description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores e",
-    )}
-    it "returns invalid without a price" do 
-      expect(product).not_to be_valid
-    end
-  end
+
 end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,58 +1,66 @@
 require 'rails_helper'
 
-describe User do 
-  
-  it "is valid with a first name, last name, email, and password" do
-    user = User.new(
-      first_name: "Jane",
-      last_name:  "Doe",
-      email:      "john@example.com",
-      password:   "foobar",
-      )
-    expect(user).to be_valid
+RSpec.describe User, type: :model do 
+  context "is valid with a first name, last name, email, and password" do
+    it "returns valid" do
+      user = User.new(
+        first_name: "Jane",
+        last_name:  "Doe",
+        email:      "john@example.com",
+        password:   "foobar",
+        )
+      expect(user).to be_valid
+    end
   end
 
   context "is invalid without an emailadress" do 
-    it "returns invalid without an emailadress" do
-      expect(User.new(password: "foobar")).not_to be_valid
-    end
-  end
-  context "is invalid without a password" do 
-    it "returns invalid without a password" do
+    it "returns invalid" do
       expect(User.new(email: "foo@bar.com")).not_to be_valid
     end
   end
-  context "is invalid when a password has less then 6 characters" do 
-    it "returns invalid if a password has less than 6 characters" do
-      expect(User.new(email: "foo@bar.com", password: "foo")).not_to be_valid
+
+  context "is invalid with a duplicate email address" do
+    before do 
+      User.create(
+        first_name: "Jane",
+        last_name: "Doe",
+        email: "j.doe@example.com",
+        password: "foobar"
+      )
+      @user = User.new(
+        first_name: "Joe",
+        last_name: "Doe",
+        email: "j.doe@example.com",
+        password: "foobar"
+      ) 
+    end
+    it "returns invalid" do 
+      @user.valid?
+      expect(@user.errors[:email]).to include("has already been taken")
     end
   end
 
-  it "is invalid with a duplicate email address" do 
-    User.create(
-      first_name: "Jane",
-      last_name: "Doe",
-      email: "j.doe@example.com",
-      password: "foobar"
-    )
-    user = User.new(
-      first_name: "Joe",
-      last_name: "Doe",
-      email: "j.doe@example.com",
-      password: "foobar"
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+  context "is invalid without a password" do 
+    it "returns invalid" do
+      expect(User.new(password: "foobar")).not_to be_valid
+    end
+  end
+  context "is invalid when a password has less then 6 characters" do 
+    it "returns invalid" do
+      expect(User.new(password: "foobar")).not_to be_valid
+    end
   end
   
-  it "returns a user's full name as a string" do 
-    user = User.new(
-      first_name: "John",
-      last_name: "Doe",
-      email: "j.doe@example.com",
-      password: "foobar"
-    )
-    expect(user.name).to eq "John Doe"
+  context "returns a user's full name" do
+    it "returns name as a string" do 
+      user = User.new(
+        first_name: "John",
+        last_name: "Doe",
+        email: "j.doe@example.com",
+        password: "foobar"
+      )
+      expect(user.name).to eq "John Doe"
+    end
   end
 
 end
