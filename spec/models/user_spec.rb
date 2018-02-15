e76dc62d1c52ee7e698286cfea16624668831776
Eveline Kooijman <e.kooijman@gmail.com>
2018-02-15 12:44:44 +0100

    password email model unit test product and user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,9 +13,19 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context "is valid with only an email and password " do
+    it "returns valid" do
+      user = User.new(
+        email:      "john@example.com",
+        password:   "foobar",
+        )
+      expect(user).to be_valid
+    end
+  end
+
   context "is invalid without an emailadress" do 
     it "returns invalid" do
-      expect(User.new(email: "foo@bar.com")).not_to be_valid
+      expect(User.new(password: "foobar")).not_to be_valid
     end
   end
 
@@ -42,12 +52,12 @@ RSpec.describe User, type: :model do
 
   context "is invalid without a password" do 
     it "returns invalid" do
-      expect(User.new(password: "foobar")).not_to be_valid
+      expect(User.new(email: "j.doe@example.com")).not_to be_valid
     end
   end
   context "is invalid when a password has less then 6 characters" do 
     it "returns invalid" do
-      expect(User.new(password: "foobar")).not_to be_valid
+      expect(User.new(email: "j.doe@example.com", password: "oobar")).not_to be_valid
     end
   end
   
