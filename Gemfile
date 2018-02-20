89e0ef347af566b4363e2fc6ee0f11f12b2c6b3d
Eveline Kooijman <e.kooijman@gmail.com>
2018-02-20 13:38:02 +0100

    Fix ighting, living, dining view on dependency with empty array

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'cancancan', '~> 2.0'
 gem 'will_paginate', '~> 3.1.1'
 
 
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,27 +7,27 @@ class StaticPagesController < ApplicationController
   end
 
   def living
-    living_categorie = Categorie.find_by(name: "Living")
-    if living_categorie.present?
-      @living_products = living_categorie.products.page(params[:page]).per_page(8)
+    @living_products = Categorie.find_by(name: "Living").products
+    if @living_products.present?
+      @living_products_paginated = @living_products.page(params[:page]).per_page(8)
     else
       @category_notice = "This category doesn\'t seem to have any products at the moment, please check back at a later date."
     end
   end 
 
   def lighting
-    lighting_categorie = Categorie.find_by(name: "Lighting")
-    if lighting_categorie.present?
-      @lighting_products = lighting_categorie.products.page(params[:page]).per_page(8)
-    else
+    @lighting_products = Categorie.find_by(name: "Lighting").products
+    if @lighting_products.present?
+      @lighting_products_paginated = @lighting_products.page(params[:page]).per_page(8)
+    elsif @lighting_products.empty?
       @category_notice = "This category doesn\'t seem to have any products at the moment, please check back at a later date."
     end
   end 
 
   def dining
-    dining_categorie = Categorie.find_by(name: "Dining")
-    if dining_categorie.present?
-      @dining_products = dining_categorie.products.page(params[:page]).per_page(8)
+   @dining_products = Categorie.find_by(name: "Dining").products
+    if @dining_products.present?
+      @dining_products_paginated = @dining_products.page(params[:page]).per_page(8)
     else
       @category_notice = "This category doesn\'t seem to have any products at the moment, please check back at a later date."
     end 
