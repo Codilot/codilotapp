ac8ef3b40a07872610616423d7bad13040976d2e
Eveline Kooijman <e.kooijman@gmail.com>
2018-02-24 12:03:50 +0100

    CF 6.6 extra | Add custom Stripe Payment integration

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,6 @@
 class PaymentsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @user = current_user
     token = params[:stripeToken]
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
@@ -27,7 +26,7 @@ class PaymentsController < ApplicationController
         body = e.json_body
         err = body[:error]
         flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-        redirect_to product_path(product)
+        redirect_to product_path(@product)
     end
     
   end
