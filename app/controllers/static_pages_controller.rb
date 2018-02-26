8ce5970d7bc21e0fbf8e6546a5985e171f9f5fea
Eveline Kooijman <e.kooijman@gmail.com>
2018-02-26 17:58:58 +0100

    CF 6.7.| Remove byebug breaking points in product controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,12 @@
 class StaticPagesController < ApplicationController
   
   def landing_page
-    byebug
     @featured_product = Product.all.sort_by { |e| e.id }.reverse[0..5]
     @product_slide_groups = Product.all.each_slice(3).to_a
     @featured_article = Article.last
   end
 
   def living
-    byebug
     @living_products = Categorie.find_by(name: "Living").products
     if @living_products.present?
       @living_products_paginated = @living_products.page(params[:page]).per_page(8)
@@ -18,7 +16,6 @@ class StaticPagesController < ApplicationController
   end 
 
   def lighting
-    byebug
     @lighting_products = Categorie.find_by(name: "Lighting").products
     if @lighting_products.present?
       @lighting_products_paginated = @lighting_products.page(params[:page]).per_page(8)
@@ -28,7 +25,6 @@ class StaticPagesController < ApplicationController
   end 
 
   def dining
-    byebug
    @dining_products = Categorie.find_by(name: "Dining").products
     if @dining_products.present?
       @dining_products_paginated = @dining_products.page(params[:page]).per_page(8)
@@ -41,7 +37,6 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    byebug
     UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
