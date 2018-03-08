78fd0651cae44f57dcfd16a5cf29f8f25c9a6282
Eveline Kooijman <e.kooijman@gmail.com>
2018-03-08 15:00:22 +0100

    CF 6.11 | Add Memcachier gem, add dalli gem, add fragment caching to articles index and product index views

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,4 +4,17 @@ module ProductsHelper
     max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "products/all-#{count}-#{max_updated_at}"
   end
+  
+  def cache_key_for_featured_products
+    count          = @featured_products
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "featured_products/all-#{count}-#{max_updated_at}"
+  end
+  
+  def cache_key_for_product_slide_groups
+    count          = @product_slide_groups
+    max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "product_slide_groups/all-#{count}-#{max_updated_at}"
+  end
+
 end
